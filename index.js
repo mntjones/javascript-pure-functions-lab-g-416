9266563f7105150d8b409b5aa079ab3e17cbd437
Monica Tamaru Jones <mntamaru@gmail.com>
2019-03-21 20:40:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,11 +31,10 @@ const getNextEpisodeInPlaylist = (playlist) => {
 function bingeWatch(playlist) {
   if (playlist.length === 0) {
     return "Please let there be more!";
-    newPlaylist.shift();
-    bingeWatch(newPlaylist);
   }
   else {
-    
+    const newPlaylist = playlist.shift();
+    return bingeWatch(newPlaylist);
   }
 }
 
